09843a58366c960563de66f0121d52a063726866
+ <+>
2025-01-04 01:04:55 -0600

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [dogData, setDogData] = useState("")
+    const URL = "https://dog.ceo/api/breeds/image/random"
+
+    useEffect(() => {
+        fetch(URL)
+            .then(response => response.json())
+            .then(data => setDogData(data.message))
+    }, [])
+
+    return (
+        <>
+            {dogData ? (
+                <img
+                    src={dogData}
+                    alt="A Random Dog"
+                ></img>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </>
+    );
+}
+
+export default App;
